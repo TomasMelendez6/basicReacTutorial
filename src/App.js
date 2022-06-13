3f63ce0eb295daabe1a7d6b7958083ed607f30c2
tmelendez <tomas.melendez@gueno.com>
2022-06-13 10:16:50 +0200

    lesson-1: video 5

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const link = "www.google.com";
-  
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times.</p>
-        <p>{ 10 }</p>
-        <p>{ "Hello" }</p>
-        <p>{ [1,2,3,4] }</p>
-        <p>{ Math.random() * 10 }</p>
+        <Home />
       </div>
     </div>
   );
