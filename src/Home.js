2a21db1d68c85eb499fea4717b4755385940ebbb
tmelendez <tomas.melendez@gueno.com>
2022-06-14 12:04:31 +0200

    lesson-12: reusing components

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,13 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'Lorem ipsum...', author: 'Mario', id: 1 },
         { title: 'Welcome party!', body: 'Lorem ipsum...', author: 'Yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'Lorem ipsum...', author: 'Luigi', id: 3 }
+        { title: 'Web dev top tips', body: 'Lorem ipsum...', author: 'Mario', id: 3 }
     ])
     
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All blogs!" />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Mario')} title="Mario's blogs!" />
         </div>
      );
 }
