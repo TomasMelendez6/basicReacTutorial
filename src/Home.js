e4c99918d20b2fd71e32aa44dd8f2523aad832b5
tmelendez <tomas.melendez@gueno.com>
2022-06-14 12:37:31 +0200

    lesson-16: using json server

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,14 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList'
 
+// execute json server: npx json-server --watch data/db.json --port 8000
+
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'Lorem ipsum...', author: 'Mario', id: 1 },
         { title: 'Welcome party!', body: 'Lorem ipsum...', author: 'Yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'Lorem ipsum...', author: 'Mario', id: 3 }
     ])
-
-    const [name, setName] = useState('Mario')
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id != id)
@@ -17,14 +17,11 @@ const Home = () => {
 
     useEffect(() => {
         console.log('use effect ran');
-        console.log(name);
-    }, [name])
+    }, [])
     
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} />
-            <button onClick={() => setName('Luigi')}>change name</button>
-            <p>{ name }</p>
         </div>
      );
 }
