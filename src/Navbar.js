dda2d5f88ce36d2e7ab81d714dfd77e6c5292dfd
tmelendez <tomas.melendez@gueno.com>
2022-06-15 12:02:09 +0200

    lesson-23: Router links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New blog</a>
+                <Link href="/">Home</Link>
+                <Link href="/create">New blog</Link>
             </div>
         </nav>
     );
