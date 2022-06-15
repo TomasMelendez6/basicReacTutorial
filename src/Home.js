af3adccdd06ac0d88a9a0c8d5754070cd1e1f166
tmelendez <tomas.melendez@gueno.com>
2022-06-15 11:35:28 +0200

    lesson-20: creating custom hooks

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,12 @@
-import { useState, useEffect } from 'react';
+import useFetch from './useFetch';
 import BlogList from './BlogList'
 
 // execute json server: npx json-server --watch data/db.json --port 8000
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-    const [isPending, setIsPending] = useState(true)
-    const [error, setError] = useState(null)
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    if(!res.ok) {
-                        throw Error('Could not fetch data for that resource');
-                    }
-                    return res.json();
-                })
-                .then(data => {
-                    setBlogs(data);
-                    setIsPending(false)
-                    setError(null)
-                })
-                .catch(err => {
-                    setIsPending(false)
-                    setError(err.message)
-                })
-    }, 1000)
-    }, [])
+    
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
+    
     
     return ( 
         <div className="home">
